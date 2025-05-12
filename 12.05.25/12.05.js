2a40eb1d83de7db5c94d58445f716b9660f05980
Арсений <Penkovskiy2024@mail.ru>
2025-05-12 21:03:07 +0300

    Add files via upload

---
+++ 12.05.25/12.05.js
@@ -0,0 +1,33 @@
+//дом
+
+// // как получить дом элементы.
+// let elbyID = document.getElementById('myID')
+// alert('elbyID')
+
+// let el = document.querySelector("#myID")
+// console.log(el)
+
+// let el = document.querySelectorAll('.myId')
+// console.log('el')
+
+// let els = document.querySelectorAll('.ClassName')
+// console.log('els')
+
+
+//Как менять DOM
+// const div = document.querySelector('hi')
+// //Измененый элемент
+// div.textContent = 'Здесь пишем новый текст'
+
+// div.style.color = 'red'
+// div.style.fontSize = '100px'
+
+//изменение атрибутов
+
+// div.setAllTributes('hi, hi2')
+// let value = div.getAlltributes('hi')
+
+//Создание и добавлнение элементов
+
+// let button  = document.querySelector('.button')
+// console.log('Ты кликнул')
---
+++ 12.05.25/12.05.py
@@ -0,0 +1,34 @@
+# name = "1"
+# name1 = 1
+# name2 = 1.2
+# name3 = True;
+
+# UserName = 'UserName'
+# user_name = '1'
+
+# Name = 1
+# Name = 2
+# print(name)
+# print('ABC')
+
+# Типы данных в python
+# bool - логическое значение
+# int,float,complex,str
+ 
+int = 1 #30,1,50 целые числа
+float = 1.2 #числа с плапвающей точкой, в качестве разделителя используется точка
+str = "Любые как в js"
+
+
+
+
+
+
+
+
+
+
+
+username = "User1"
+print(f"Пользователь {username}")
+print(type(username)
