38545ed27b2c986faad0bda4d86a9baa964e2e65
Arsenii Penkovski <Penkovskiy2024@mail.ru>
2025-05-12 21:01:19 +0300

    first commit

---
+++ 12.05.py
@@ -0,0 +1,79 @@
+
+# name = "1"
+# name1 = 1
+# name2 = 1.2
+# name3 = True;
+
+# UserName = 'UserName'
+# user_name = '1'
+
+# Name = 1
+# Name = 2
+# print(name)
+# print('ABC')
+
+# Типы данных в python
+# bool - логическое значение
+# int,float,complex,str
+ 
+# int = 1 #30,1,50 целые числа
+# float = 1.2 #числа с плапвающей точкой, в качестве разделителя используется точка
+# str = "Любые как в js"
+
+
+# # Управляющие последовательности в строка
+# # /n- осуществляет переход в новую строку
+# # /t - добавляеь табуляцию
+
+
+
+# username = "User1"
+# print(f"Пользователь {username}")
+# print(type(username)
+      
+# request = int(input ("Введите число 1: "))
+# request1 = int(input ("Введите число 2: "))
+# math = request + request1
+# print(math)
+
+# number = 4.3
+# print(round(number))
+
+# операции сравнения
+#  == возвращает true если оба операнда равны.Иначе возвращает False
+#  != возвращает true если оба операнда не равны, иначе false
+# a = 5
+# b = 6
+# result=a==b
+# print(result)
+
+# Логические операции
+# age = 22
+# weight = 300
+# result = age > 21 or weight == 300
+# result = age > 21 and weight == 300
+
+# language = "russia"
+# if language = "german":
+#     print("Немецкий не русский")
+# elif language == "english"
+#     print("Работает второе условие")
+
+# time = int(input("Введите время"))
+# if time > 22 and time < 6:
+#     print("Иди спать")
+# elif time > 0 or time < 12:
+#     print("Утро")
+# else:
+#     print("Это блок else")
+
+
+# циклы
+while = работает пока условие истина
+    while number < 5:
+        print(f"number = {number}")
+        number +=1
+print("Работа программы завершена")
+
+for n in range(0, 500):
+    print(n)
