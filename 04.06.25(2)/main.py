b7cf86e469482b299867b50f7aebb860ebd81973
Арсений <Penkovskiy2024@mail.ru>
2025-06-04 20:55:38 +0300

    Add files via upload

---
+++ 04.06.25(2)/main.py
@@ -0,0 +1,5 @@
+# from students import students
+
+import students as python47
+
+print(python47.students)
---
+++ 04.06.25(2)/students.py
@@ -0,0 +1,17 @@
+# students = {
+#     'students1':'Илья Пестеров'
+#     'students2':'Марта'
+#     'students3':'Вазген'
+#     'students4':'Эля'
+# }
+
+# def printDict(stud):
+#     print(f"Список студентов группы python47:",stud)
+#     printDict(students);
+
+# def main():
+#     printDict(students)
+
+# if __name__=="__main__":
+#     main()
+
